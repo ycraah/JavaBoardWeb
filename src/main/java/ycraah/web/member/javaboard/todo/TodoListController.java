e51975301e75aa111860af91e9bc1f2b87096beb
ycraah <ycraah@gmail.com>
2024-08-24 10:24:10 +0900

    todo 리스트 실행 확인

--- src/main/java/ycraah/web/member/javaboard/todo/TodoListController.java
+++ src/main/java/ycraah/web/member/javaboard/todo/TodoListController.java
@@ -6,14 +6,20 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ycraah.web.member.javaboard.todo.dto.TodoDTO;
+import ycraah.web.member.javaboard.todo.service.TodoService;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/todo/list")
 public class TodoListController extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     System.out.println("/todo/list 실행");
+    List<TodoDTO> todoList = TodoService.INSTANCE.getList();
+    req.setAttribute("todoList", todoList);
+
     RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/todo/list.jsp");
     dispatcher.forward(req, resp);
   }
--- src/main/webapp/WEB-INF/todo/list.jsp
+++ src/main/webapp/WEB-INF/todo/list.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-    <h1>List Page</h1>
+    ${todoList}
 </body>
 </html>
