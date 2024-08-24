9c0ab54b8d3bb0d7d313f8ddc703534f76a9a4c1
ycraah <ycraah@gmail.com>
2024-08-24 10:09:05 +0900

    todoregister 컨트롤러 도입, jsp 작동 확인

---
+++ src/main/java/ycraah/web/member/javaboard/todo/TodoReigsterController.java
@@ -0,0 +1,24 @@
+package ycraah.web.member.javaboard.todo;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/todo/register")
+public class TodoReigsterController extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    System.out.println("입력 화면을 볼 수 있도록 구성");
+
+    RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/todo/register.jsp");
+    dispatcher.forward(req, resp);
+  }
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    System.out.println("입력을 처리하고 목록 페이지로 이동");
+  }
+}
--- src/main/webapp/WEB-INF/todo/register.jsp
+++ src/main/webapp/WEB-INF/todo/register.jsp
@@ -11,6 +11,8 @@
     <title>Title</title>
 </head>
 <body>
-
+    <form action="/todo/register" method="post">
+        <button type="submit">등록 처리</button>
+    </form>
 </body>
 </html>
